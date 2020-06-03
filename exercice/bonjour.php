68bb6ad15cdc371278dddd3f1eee904bdeac25d6
GretaMultimedia <greta.multimedia@gmail.com>
2020-06-03 10:56:42 +0200

    Update bonjour.php

--- exercice/bonjour.php
+++ exercice/bonjour.php
@@ -1,17 +1,23 @@
 <?php
+/*
+    Commentaire
+    multiligne
+*/
 
 //Récupération des infos du formulaire
 
-$nom=$_POST["nom"];
-$prenom=$_POST["prenom"];
-$age=$_POST["age"];
+$nom=$_POST["nom"];         //recupération du nom
+$prenom=$_POST["prenom"];   //recupération du prénom
+$age=$_POST["age"];         //recupération de l'age
 
 
 //Definition du status
-
 if ($age>=18){
     $status="majeur";   
 }
 else{
     $status="mineur";   
 }
+
+
+echo "Bonjour $prenom $nom.<br>Vous êtes $status.";
